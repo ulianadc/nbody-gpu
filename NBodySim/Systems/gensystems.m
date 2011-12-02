24a152063e5d14c1838dc5249b6604e5fe705bbc
David Uliana <ulianadc@gmail.com>
2011-12-02 18:56:57 -0500

    Generate ASCII files as well.

--- NBodySim/Systems/gensystems.m
+++ NBodySim/Systems/gensystems.m
@@ -5,10 +5,10 @@ for i=systemSizes
     system = [ random('norm', 0, 1e3, [i 3]).*(1e5), ...
                random('norm', 0, 1e2, [i 3]), ...
                random('norm', 1e18, 1e15, [i 1]) ];
+    dlmwrite(sprintf('system%06d.txt', i), system, '\t');
     fid = fopen(sprintf('system%06d.dat', i), 'w');
     fwrite(fid, system', 'float');
     fclose(fid);
-    dlmwrite(sprintf('system%06d.txt', i), system, '\t');
     waitbar(i/max(systemSizes), wbh);
 end
 close(wbh);
