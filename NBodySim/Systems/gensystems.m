0be8b5ae4edcb119c302421209d7cf60c83b0570
David Uliana <ulianadc@gmail.com>
2011-12-02 18:53:53 -0500

    Add new set of random systems in both ASCII and binary format.

--- NBodySim/Systems/gensystems.m
+++ NBodySim/Systems/gensystems.m
@@ -1,6 +1,6 @@
 clear,clc
 wbh = waitbar(0, 'Generating systems...');
-systemSizes = (1:16).*(2^14);
+systemSizes = (2^13:2^13:2^17);
 for i=systemSizes
     system = [ random('norm', 0, 1e3, [i 3]).*(1e5), ...
                random('norm', 0, 1e2, [i 3]), ...
@@ -8,6 +8,7 @@ for i=systemSizes
     fid = fopen(sprintf('system%06d.dat', i), 'w');
     fwrite(fid, system', 'float');
     fclose(fid);
+    dlmwrite(sprintf('system%06d.txt', i), system, '\t');
     waitbar(i/max(systemSizes), wbh);
 end
 close(wbh);
